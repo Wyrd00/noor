b03a087f7a4dcfacfc11aef753e36b52cfdc0c1d
matthewdking <matthew.d.king@outlook.com>
2017-09-26 11:11:11 +0300

    adds navbar to all pages relates #21

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,12 @@
 var english = document.querySelector('#english');
 var arabic = document.querySelector('#arabic');
 var hebrew = document.querySelector('#hebrew');
+var faq = document.querySelector('#FAQ');
+var tools = document.querySelector('#tools');
+var videos = document.querySelector('#videos');
+var articles = document.querySelector('#articles');
+var newContent = document.querySelector('#new');
+var home = document.querySelector('#home');
 
 english.addEventListener('click', function () {
   window.location = 'index-eng.html';
@@ -13,3 +19,27 @@ arabic.addEventListener('click', function () {
 hebrew.addEventListener('click', function () {
   window.location = 'index-heb.html';
 });
+
+faq.addEventListener('click', function () {
+  window.location = 'FAQ.html';
+});
+
+tools.addEventListener('click', function () {
+  window.location = 'categoryEquipments.html'
+})
+
+videos.addEventListener('click', function () {
+  window.location = 'categoryVideos.html'
+})
+
+articles.addEventListener('click', function () {
+  window.location = 'categoryArticles.html'
+})
+
+newContent.addEventListener('click', function () {
+  window.location = 'new.html'
+})
+
+home.addEventListener('click', function () {
+  window.location = 'index-arb.html'
+})
